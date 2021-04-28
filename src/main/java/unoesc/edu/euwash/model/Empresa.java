dab4778c2b780b4bbbc4f126fd1e775f61ee95d2
Daniel Seben <daniel.souza@unoesc.edu.br>
2021-04-27 21:48:14 -0300

    Organizacao MVC

---
+++ src/main/java/unoesc/edu/euwash/model/Empresa.java
@@ -0,0 +1,44 @@
+package unoesc.edu.euwash.model;
+
+public class Empresa {
+	private int id;
+	private String razaoSocial;
+	private String nomeFantasia;
+	private String cnpj;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@page import="unoesc.edu.euwash.model.Empresa"%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MENU</title>
+</head>
+<body>
+
+<li><a href="empresa"> Empresa</li>
+
+
+</body>
+</html>
